6837c4d701df4df93e64c801946676ec7a164181
Elson Otake <elsonotake@yahoo.com.br>
2022-08-08 19:42:24 -0300

    Add create database and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(100) NOT NULL,
+  date_of_birth date,
+  escape_attempts integer,
+  neutered boolean,
+  weight_kg decimal
 );
