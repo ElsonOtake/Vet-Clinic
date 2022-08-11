3d9050f2b82e8fc5c10b893239ad04e99a808bef
Elson Otake <elsonotake@yahoo.com.br>
2022-08-11 14:38:35 -0300

    Add vets table and join tables

--- schema.sql
+++ schema.sql
@@ -31,3 +31,24 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id integer REFERENCES species (id);
 
 ALTER TABLE animals ADD COLUMN owner_id integer REFERENCES owners (id);
+
+CREATE TABLE vets (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  name varchar(100) NOT NULL,
+  age integer,
+  date_of_graduation date,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+  vet_id integer REFERENCES vets (id),
+  specie_id integer REFERENCES species (id),
+  PRIMARY KEY (vet_id, specie_id)
+);
+
+CREATE TABLE visits (
+  vet_id integer REFERENCES vets (id),
+  animal_id integer REFERENCES animals (id),
+  date_of_visit date,
+  PRIMARY KEY (vet_id, animal_id)
+);
