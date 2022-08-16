6a0f053aa821ee2ab2695b2ec19c693d69223a30
Elson Otake <elsonotake@yahoo.com.br>
2022-08-16 17:24:00 -0300

    Add join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,31 +7,21 @@ CREATE TABLE patients (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE medical_histories (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  patient_id integer REFERENCES patients(id),
+  status varchar(255),
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE invoices (
   id integer GENERATED ALWAYS AS IDENTITY,
   total_amount DECIMAL NOT NULL,
   generated_at TIMESTAMP NOT NULL, 
   payed_at TIMESTAMP NOT NULL,
-  medical_history_id INT NOT NULL,
+  medical_history_id INT NOT NULL REFERENCES medical_histories(id),
   PRIMARY KEY (id)
-);
-
-CREATE TABLE inovice_items (
-  id integer GENERATED ALWAYS AS IDENTITY,
-  unit_price DECIMAL,
-  quantity integer,
-  total_price DECIMAL,
-  invoice_id integer,
-  treatment_id integer,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE medical_histories (
-  id integer GENERATED ALWAYS AS IDENTITY,
-  admitted_at TIMESTAMP,
-  patient_id integer,
-  status varchar(255),
-  PRIMARY KEY(id)
 );
 
 CREATE TABLE treatments (
@@ -39,4 +29,20 @@ CREATE TABLE treatments (
   type VARCHAR(255),
   name VARCHAR(255),
   primary key (id)
+);
+
+CREATE TABLE invoice_items (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity integer,
+  total_price DECIMAL,
+  invoice_id integer REFERENCES invoices(id),
+  treatment_id integer REFERENCES treatments(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_treatments (
+  medical_id integer REFERENCES medical_histories(id),
+  treatment_id integer REFERENCES treatments(id),
+  PRIMARY KEY (medical_id, treatment_id)
 );
